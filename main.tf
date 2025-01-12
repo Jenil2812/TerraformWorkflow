16ea1251852be252632696309837639112bf2b61
Jenil Gandhi <142617735+Jenil2812@users.noreply.github.com>
2025-01-12 09:50:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "azurerm" {
   features {}
 }
 
+# Resource Group for ACR
 resource "azurerm_resource_group" "default" {
   name     = "container-registry-rg"
   location = "Canada Central"
@@ -11,14 +12,38 @@ resource "azurerm_resource_group" "default" {
   }
 }
 
+# Azure Container Registry
 resource "azurerm_container_registry" "acr" {
   name                = "demotryterraformstore"
   resource_group_name = azurerm_resource_group.default.name
-  location            = "Canada Central"
+  location            = azurerm_resource_group.default.location
   sku                 = "Standard"
   admin_enabled       = true
 }
 
+# Resource Group for Backend Storage
+resource "azurerm_resource_group" "tfstates" {
+  name     = "tfstates"
+  location = "Canada Central"
+}
+
+# Storage Account for Terraform State
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "kstfstateaccount111111"
+  resource_group_name      = azurerm_resource_group.tfstates.name
+  location                 = azurerm_resource_group.tfstates.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Storage Container for Terraform State
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+# Outputs for ACR
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
@@ -31,4 +56,4 @@ output "acr_admin_username" {
 output "acr_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
-}   
+}
